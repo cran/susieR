28fbcdebc77f807106e6ec131d45292ca51813f3
Peter Carbonetto <peter.carbonetto@gmail.com>
2022-08-20 06:10:08 +0000

    version 0.12.19

--- inst/doc/trend_filtering.R
+++ inst/doc/trend_filtering.R
@@ -26,31 +26,30 @@ plot(y,pch=".")
 lines(mu,col=1,lwd=3)
 lines(predict(s),col=2,lwd=2)
 
-## -----------------------------------------------------------------------------
-library(genlasso)
-y.tf = trendfilter(y,ord=0)
-y.tf.cv = cv.trendfilter(y.tf)
+## ---- eval=FALSE--------------------------------------------------------------
+#  # install.packages("remotes")
+#  # remotes::install_github("glmgen/genlasso")
+#  y.tf = trendfilter(y,ord=0)
+#  y.tf.cv = cv.trendfilter(y.tf)
+#  plot(y,pch=".")
+#  lines(mu,col=1,lwd=3)
+#  lines(predict(s),col=2,lwd=2)
+#  lines(y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)],col=4,lwd=2)
 
-plot(y,pch=".")
-lines(mu,col=1,lwd=3)
-lines(predict(s),col=2,lwd=2)
-lines(y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)],col=4,lwd=2)
+## ---- eval=FALSE--------------------------------------------------------------
+#  set.seed(1)
+#  mu = seq(0,4,length=1000)
+#  mu = mu + c(rep(0,500),rep(4,500))
+#  y = mu + rnorm(n)
+#  s = susie_trendfilter(y,0,L=10)
+#  y.tf = trendfilter(y,ord=0)
+#  y.tf.cv = cv.trendfilter(y.tf)
+#  plot(y,pch=".")
+#  lines(mu,col=1,lwd=3)
+#  lines(predict(s),col=2,lwd=2)
+#  lines(y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)],col=4,lwd=2)
 
-## -----------------------------------------------------------------------------
-set.seed(1)
-mu = seq(0,4,length=1000)
-mu = mu + c(rep(0,500),rep(4,500))
-y = mu + rnorm(n)
-s = susie_trendfilter(y,0,L=10)
-y.tf = trendfilter(y,ord=0)
-y.tf.cv = cv.trendfilter(y.tf)
-
-plot(y,pch=".")
-lines(mu,col=1,lwd=3)
-lines(predict(s),col=2,lwd=2)
-lines(y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)],col=4,lwd=2)
-
-## -----------------------------------------------------------------------------
-sqrt(mean((mu-y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)])^2))
-sqrt(mean((mu-predict(s))^2))
+## ---- eval=FALSE--------------------------------------------------------------
+#  sqrt(mean((mu-y.tf$fit[,which(y.tf$lambda==y.tf.cv$lambda.min)])^2))
+#  sqrt(mean((mu-predict(s))^2))
 
